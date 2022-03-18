1b8827b4ee5541d1701a898c7d4ed947d749c5bc
Deepak Jayanna <88205314+djayanna@users.noreply.github.com>
2022-03-17 22:36:24 -0400

    Handle edge case where queue.tasks_by_status is not initialized.

--- src/helpers/QueueStatsHelper.js
+++ src/helpers/QueueStatsHelper.js
@@ -2,11 +2,22 @@
 export default class QueueStatsHelper {
     
     static getActiveTasks(queueList) {
-       return  Object.values(queueList).reduce((acc, item) => acc + item.tasks_by_status.assigned + item.tasks_by_status.wrapping, 0)
+       return  Object.values(queueList).reduce((acc, item) => {
+    
+            const assigned =  item.tasks_by_status?.assigned || 0;
+            const wrapping =  item.tasks_by_status?.wrapping || 0;
+            
+            return acc + assigned + wrapping
+       }, 0);
     }
 
     static getWaitingTasks(queueList) {
-        return  Object.values(queueList).reduce((acc, item) => acc + item.tasks_by_status.reserved + item.tasks_by_status.pending, 0)
+        return  Object.values(queueList).reduce((acc, item) =>
+        {   
+            const reserved =  item.tasks_by_status?.reserved || 0;
+            const pending =  item.tasks_by_status?.pending || 0;
+            
+            return acc + reserved + pending
+       }, 0);
      }
-
 }
